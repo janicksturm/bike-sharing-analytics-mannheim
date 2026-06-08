bb962d43130ec5d66782dc30d6119840865c0d92
Janick Sturm <sturmjanick@gmx.net>
2026-06-08 22:21:19 +0200

    Enhance functions with description comments

--- script/models/preprocessing.py
+++ script/models/preprocessing.py
@@ -42,19 +42,33 @@ def features(df: pd.DataFrame) -> pd.DataFrame:
 
     df["total_capacity"] = df["bikes"] + df["free_racks"]
 
+    # We want to calculate how full each bike station is. 
+    # First, we find out the total number of spaces by adding the number of bikes currently there and the number of free racks. 
+    # Then, we calculate the percentage of those spaces that are occupied by bikes. 
+    # If there are no spaces available (total capacity is zero), we avoid dividing by zero 
+    # by only calculating the percentage where the total capacity is greater than zero. 
     _capacity = df["total_capacity"].astype(float)
     df["occupancy_pct"] = (
         (df["bikes"] / _capacity.where(_capacity > 0) * 100)
         .round(1)
     )
 
+    # We want to categorize the bike availability.
+    # If there are no bikes available, we label it as "Empty". 
+    # If there are 1 or 2 bikes available, we label it as "Low". 
+    # If there are more than 2 bikes available, we label it as "Available".
     df["status"] = df["bikes"].apply(
         lambda b: "Empty" if b == 0 else ("Low" if b <= 2 else "Available")
     )
 
+    # We want to capture the change in bike availability at each station over time.
+    # We calculate the difference in the number of bikes at each station between consecutive snapshots.
+    # A negative difference (more bikes taken out) indicates higher demand, while a positive difference (more bikes returned) indicates lower demand. 
     df["bike_delta"] = df.groupby("uid")["bikes"].diff()
 
-
+    # To create a demand score, we take the negative of the bike delta. 
+    # This way, a higher demand (more bikes taken out) will result in a higher demand score, 
+    # while a lower demand (more bikes returned) will result in a lower demand score.
     df["demand_score"] = -df["bike_delta"]
 
     return df
