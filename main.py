93f8a9dee323698febdac72dc3fffde2a3aeab58
Janick Sturm <sturmjanick@gmx.net>
2026-06-08 21:37:26 +0200

    Refactor main.py and add webserver

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from script.pipe.fetch_data import download_file
 import time
+import threading
+import uvicorn
+
+from script.pipe.fetch_data import download_file
 
 def run_pipeline():
     try:
         download_file()
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Pipeline Error: {e}")
 
-
-# run every 30 minutes
-if __name__ == "__main__":
+def pipeline_loop():
     while True:
         run_pipeline()
         time.sleep(1800)
+
+def start_api():
+    uvicorn.run(
+        "script.api.app:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=False
+    )
+
+if __name__ == "__main__":
+    pipeline_thread = threading.Thread(
+        target=pipeline_loop,
+        daemon=True
+    )
+    pipeline_thread.start()
+
+    start_api()
