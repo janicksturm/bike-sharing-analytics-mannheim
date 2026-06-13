525f31295cdb98641bb06ecc5a0ca7f43449752f
Janick Sturm <sturmjanick@gmx.net>
2026-06-13 16:50:13 +0200

    feat: implement custom useFetch hook for data fetching

---
+++ dashboard/src/hooks/useFetch.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+const useFetch = <T,>(url: string) => {
+  const [data, setData] = useState<T[] | null>(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [url]);
+
+  return [data] as const;
+};
+
+export default useFetch;
