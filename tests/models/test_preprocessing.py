ca6e080abcd843b51aad751472e6f5356cfe17c9
Janick Sturm <sturmjanick@gmx.net>
2026-07-17 12:55:09 +0200

    test: enhance feature tests

--- tests/models/test_preprocessing.py
+++ tests/models/test_preprocessing.py
@@ -1,4 +1,15 @@
-from script.models.preprocessing import load_all_snapshots
+import pytest
+import pandas as pd
+from script.models.preprocessing import load_all_snapshots, features
+
+
+@pytest.fixture
+def raw_station_data():
+    return pd.DataFrame({
+        "uid": [339462, 660945, 339343],
+        "bikes": [4, 0, 18],
+        "free_racks": [4, 8, 0]
+    })
 
 def test_dataframe_header():
     expected_columns = [
@@ -10,3 +21,16 @@ def test_dataframe_header():
     df = load_all_snapshots()
 
     assert df.columns.to_list() == expected_columns
+
+
+def test_features_addition(raw_station_data):
+
+    result_df = features(raw_station_data)
+    
+    assert "total_capacity" in result_df.columns
+    assert "status" in result_df.columns
+    assert "demand_score" in result_df.columns
+    
+
+    assert result_df.loc[0, "status"] == "Available"
+    assert result_df.loc[1, "status"] == "Empty"
