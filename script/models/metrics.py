308d823f035854b9b448b2629e4f8b85255afb21
Janick Sturm <sturmjanick@gmx.net>
2026-07-17 12:36:19 +0200

    fix: change return type

--- script/models/metrics.py
+++ script/models/metrics.py
@@ -16,7 +16,7 @@ def calculate_empty_rate(df: pd.DataFrame) -> pd.DataFrame:
 # Then, we calculate the percentage of those spaces that are occupied by bikes. 
 # If there are no spaces available (total capacity is zero), we avoid dividing by zero 
 # by only calculating the percentage where the total capacity is greater than zero. 
-def calculate_station_occupancy_capacity(df: pd.DataFrame) -> pd.DataFrame:
+def calculate_station_occupancy_capacity(df: pd.DataFrame) -> pd.Series:
     _capacity = df["total_capacity"].astype(float)
     return (
         (df["bikes"] / _capacity.where(_capacity > 0) * 100)
